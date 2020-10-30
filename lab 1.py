326069d038ebfc6e7703fdde09ce92d3873298e2
febri aditiya <aditiya022002@gmail.com>
2020-10-30 15:37:35 +0700

    menambahkan file

---
+++ lab 1.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 30 13:39:34 2020
+
+@author: febri aditiya
+"""
+print ("=================")
+print ("    BIODATA")
+print ("=================")
+
+nama = input("nama lengkap: ")
+nama2 = input("nama panggilan: ")
+npm = input("masukkan npm: ")
+tpl = input("tempat lahir: ")
+tgl = input("tanggal lahir: ")
+tpln = input("masukkan nomor telepon: ")
+almet = input("masukkan alamat: ")
+
+
+#print
+
+print ("=================")
+print ("    BIODATA SAYA ")
+print ("=================")
+
+print("Assalamu'alaikum.")
+
+print("hello",nama,"nama panggilan saya",nama2,"no npm saya",npm,)
+print("saya lahir dari kota",tpl,"dan saya lahir pada tanngal",tgl,)
+print("nomor telepon saya adalah",tpln,"saya tinggal di daerah",almet)
